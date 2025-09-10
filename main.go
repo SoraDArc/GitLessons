ca90674b63fbc9de7b8baf7f5ab11f788456675c
SoraDArc <SyaDmitry@gmail.com>
2025-09-10 23:59:47 +0800

    concurrency test

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"project/worker"
+	"sync"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World!")
-	fmt.Println("Nihao world!")
-	fmt.Println("Privet world!")
+	workerContext, workerCancel := context.WithCancel(context.Background())
+
+	timeInit := time.Now()
+	taskSetPoint := worker.WorkerPool(workerContext, 100)
+	time.Sleep(3 * time.Second)
+	workerCancel()
+
+	var mtx sync.Mutex
+	var wg sync.WaitGroup
+	taskResult := 0
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for val := range taskSetPoint {
+			mtx.Lock()
+			taskResult += val
+			mtx.Unlock()
+		}
+	}()
+	wg.Wait()
+
+	fmt.Println("Программа завершилась! Итого работ выполнено:", taskResult)
+	fmt.Println("Программа работала:", time.Since(timeInit))
 }
