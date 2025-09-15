a2e73ffc55613b477bb81e8d3c1154d7291e99f7
SoraDArc <SyaDmitry@gmail.com>
2025-09-16 03:11:21 +0800

    commit for cancel

--- main.go
+++ main.go
@@ -2,34 +2,10 @@ package main
 
 import (
 	"fmt"
-	"slices"
 )
 
 func main() {
 
-	var graph [][]int = [][]int{}
-	var start int
-	var used []bool = make([]bool, len(graph), len(graph))
-	parent := make([]int, len(graph), len(graph))
-	queue := make([]int, 1)
-	queue[0] = start
-	used[start] = true
-	for len(queue) > 0 {
-		v := queue[0]
-		queue = queue[1:]
-		for _, n := range graph[v] {
-			if !used[n] {
-				queue = append(queue, n)
-				parent[n] = v
-			}
-		}
-	}
-
-	var res []int
-	var end int
-	for v := end; v != -1; v = parent[v] {
-		res = append(res, v)
-	}
-	slices.Reverse(res)
-	fmt.Println(res)
+	fmt.Println("Hello world")
+	fmt.Println("nihao")
 }
