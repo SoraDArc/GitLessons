de8f7f5e87e79e113b89f2042b8c21c44a6087b2
SoraDArc <SyaDmitry@gmail.com>
2025-09-16 04:18:33 +0800

    tree struct

--- main.go
+++ main.go
@@ -1,35 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"slices"
-)
+import "fmt"
+
+type TreeNode struct {
+	Val       int
+	LeftNode  *TreeNode
+	RightNode *TreeNode
+}
 
 func main() {
-
-	var graph [][]int = [][]int{}
-	var start int
-	var used []bool = make([]bool, len(graph), len(graph))
-	parent := make([]int, len(graph), len(graph))
-	queue := make([]int, 1)
-	queue[0] = start
-	used[start] = true
-	for len(queue) > 0 {
-		v := queue[0]
-		queue = queue[1:]
-		for _, n := range graph[v] {
-			if !used[n] {
-				queue = append(queue, n)
-				parent[n] = v
-			}
-		}
+	node := TreeNode{
+		Val:       1,
+		LeftNode:  &TreeNode{Val: 2},
+		RightNode: &TreeNode{Val: 3},
 	}
 
-	var res []int
-	var end int
-	for v := end; v != -1; v = parent[v] {
-		res = append(res, v)
-	}
-	slices.Reverse(res)
-	fmt.Println(res)
+	fmt.Println(node.Val, node.LeftNode.Val, node.RightNode.Val)
 }
