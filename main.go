d335e06443477f9fe6197886aa44aa38230e8bc2
SoraDArc <SyaDmitry@gmail.com>
2025-09-16 04:41:56 +0800

    hard think

--- main.go
+++ main.go
@@ -2,18 +2,6 @@ package main
 
 import "fmt"
 
-type TreeNode struct {
-	Val       int
-	LeftNode  *TreeNode
-	RightNode *TreeNode
-}
-
 func main() {
-	node := TreeNode{
-		Val:       1,
-		LeftNode:  &TreeNode{Val: 2},
-		RightNode: &TreeNode{Val: 3},
-	}
-
-	fmt.Println(node.Val, node.LeftNode.Val, node.RightNode.Val)
+	fmt.Println("Its hard to think")
 }
